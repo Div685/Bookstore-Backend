e816e29e685394404792b147ce715bd212efc7f9
Div685 <divyeshpatel685@gmail.com>
2021-05-10 23:30:05 +0300

    update category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show]
+  before_action :set_category, only: %i[show update destroy]
 
   def index
     @categories = Category.all
@@ -10,7 +10,30 @@ class CategoriesController < ApplicationController
     json_response(@category)
   end
 
+  def create
+    @category = Category.create!(category_params)
+    json_response(@category, :created)
+  end
+
+  def show
+    json_response(@category)
+  end
+
+  def update
+    @category.update(category_params)
+    head :no_content
+  end
+
+  def destroy
+    @category.destroy
+    head :no_content
+  end
+
   private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 
   def set_category
     @category = Category.find(params[:id])

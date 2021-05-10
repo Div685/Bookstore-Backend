89865945301c45afac1d46c0457ea885c87304d1
Div685 <divyeshpatel685@gmail.com>
2021-05-10 16:49:24 +0300

    update book and categories controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,39 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :update, :destroy]
 
   # GET /books
   def index
     @books = Book.all
     json_response(@books)
+  end
+
+  def create
+    @book = Book.create!(book_params)
+    json_response(@book, :created)
+  end
+
+  def show
+    # @book = Book.find(params[:id])
+    json_response(@book)
+  end
+
+  def update
+    @book.update(book_params)
+    head :no_content
+  end
+
+  def destroy
+    @book.destroy
+    head :no_content
+  end
+
+  private
+
+  def book_params 
+    params.require(:book).permit(:title, :author, :category_id)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,19 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+
+  def index
+    @categories = Category.all
+    json_response(@categories)
+  end
+
+  def show
+    json_response(@category)
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
 end
